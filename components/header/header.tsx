74241c2e3837d395aa43cdd014052967e526a821
Nazmul Alam <nazmuldipu@gmail.com>
2022-06-05 06:33:50 +0600

    header updated

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,7 +1,6 @@
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
-import { PlusSmIcon } from '@heroicons/react/solid'
-import { Fragment } from 'react'
+import { Disclosure } from '@headlessui/react'
+import { FaGithub, FaLinkedin, FaBars, FaTimes } from 'react-icons/fa';
+import Link from 'next/link'
 
 function classNames(...classes: string[]) {
     return classes.filter(Boolean).join(' ')
@@ -20,24 +19,28 @@ export default function Header() {
                                     <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                                         <span className="sr-only">Open main menu</span>
                                         {open ? (
-                                            <XIcon className="block h-6 w-6" aria-hidden="true" />
+                                            <FaTimes className="block h-6 w-6" aria-hidden="true" />
                                         ) : (
-                                            <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                                            <FaBars className="block h-6 w-6" aria-hidden="true" />
                                         )}
                                     </Disclosure.Button>
                                 </div>
                                 <div className="flex-shrink-0 flex items-center">
                                     <img
                                         className="block lg:hidden h-8 w-auto"
-                                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                                        src="./nazmul.svg"
                                         alt="Workflow"
                                     />
                                     <img
                                         className="hidden lg:block h-8 w-auto"
-                                        src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg"
+                                        src="./nazmul.svg"
                                         alt="Workflow"
                                     />
                                 </div>
+                            </div>
+                            <div className="flex">
+
+
                                 <div className="hidden md:ml-6 md:flex md:space-x-8">
                                     {/* Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
                                     <a
@@ -67,88 +70,31 @@ export default function Header() {
                                 </div>
                             </div>
                             <div className="flex items-center">
-                                <div className="flex-shrink-0">
-                                    <button
-                                        type="button"
-                                        className="relative inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                                    >
-                                        <span>Download CV</span>
-                                    </button>
-                                </div>
-                                <div className="hidden md:ml-4 md:flex-shrink-0 md:flex md:items-center">
-                                    <button
-                                        type="button"
+                                <div className="hidden md:ml-4 md:flex-shrink-0 md:flex md:items-center mr-4">
+                                    <a
                                         className="bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                        href="https://www.linkedin.com/in/nazmuldipu/"
+                                        aria-label="LinkedIn"
+                                        target="_blank"
+                                    >
+                                        <FaLinkedin />
+                                    </a>
+                                    <a
+                                        className="bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                        href="https://github.com/nazmuldipu"
+                                        aria-label="GitHub"
+                                        target="_blank"
                                     >
                                         <span className="sr-only">View notifications</span>
-                                        <BellIcon className="h-6 w-6" aria-hidden="true" />
-                                    </button>
-
-                                    {/* Profile dropdown */}
-                                    <Menu as="div" className="ml-3 relative">
-                                        <div>
-                                            <Menu.Button className="bg-white rounded-full flex text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                                <span className="sr-only">Open user menu</span>
-                                                <img
-                                                    className="h-8 w-8 rounded-full"
-                                                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                                    alt=""
-                                                />
-                                            </Menu.Button>
-                                        </div>
-                                        <Transition
-                                            as={Fragment}
-                                            enter="transition ease-out duration-200"
-                                            enterFrom="transform opacity-0 scale-95"
-                                            enterTo="transform opacity-100 scale-100"
-                                            leave="transition ease-in duration-75"
-                                            leaveFrom="transform opacity-100 scale-100"
-                                            leaveTo="transform opacity-0 scale-95"
-                                        >
-                                            <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                                <Menu.Item>
-                                                    {({ active }) => (
-                                                        <a
-                                                            href="#"
-                                                            className={classNames(
-                                                                active ? 'bg-gray-100' : '',
-                                                                'block px-4 py-2 text-sm text-gray-700'
-                                                            )}
-                                                        >
-                                                            Your Profile
-                                                        </a>
-                                                    )}
-                                                </Menu.Item>
-                                                <Menu.Item>
-                                                    {({ active }) => (
-                                                        <a
-                                                            href="#"
-                                                            className={classNames(
-                                                                active ? 'bg-gray-100' : '',
-                                                                'block px-4 py-2 text-sm text-gray-700'
-                                                            )}
-                                                        >
-                                                            Settings
-                                                        </a>
-                                                    )}
-                                                </Menu.Item>
-                                                <Menu.Item>
-                                                    {({ active }) => (
-                                                        <a
-                                                            href="#"
-                                                            className={classNames(
-                                                                active ? 'bg-gray-100' : '',
-                                                                'block px-4 py-2 text-sm text-gray-700'
-                                                            )}
-                                                        >
-                                                            Sign out
-                                                        </a>
-                                                    )}
-                                                </Menu.Item>
-                                            </Menu.Items>
-                                        </Transition>
-                                    </Menu>
+                                        <FaGithub />
+                                    </a>
                                 </div>
+                                <div className="flex-shrink-0">
+                                    <Link href="./nazmul_alam_cv.pdf" >
+                                        <a className="relative inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"><span>Download CV</span></a>
+                                    </Link>                                   
+                                </div>
+
                             </div>
                         </div>
                     </div>
@@ -185,7 +131,7 @@ export default function Header() {
                                 CV
                             </Disclosure.Button>
                         </div>
-                        
+
                     </Disclosure.Panel>
                 </>
             )}
