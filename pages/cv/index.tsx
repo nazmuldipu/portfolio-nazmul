ba84c05fb18a1caf85fcb4224724cbb0cf9ce332
Nazmul Alam <nazmuldipu@gmail.com>
2022-07-11 12:49:35 +0600

    bug fixed

--- pages/cv/index.tsx
+++ pages/cv/index.tsx
@@ -137,7 +137,7 @@ export default function CV() {
                                         <svg className="w-2 h-2 text-blue-600 dark:text-blue-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clipRule="evenodd"></path></svg>
                                     </span>
                                     <h3 className="flex items-center text-lg font-semibold text-gray-900 dark:text-white">{item.title}</h3>
-                                    <div className="pb-1"><a className="uppercase font-secondary font-light tracking-wider" href={item.link} target="_blank">{item.company}, </a>{item.location}</div>
+                                    <div className="pb-1"><a className="uppercase font-secondary font-light tracking-wider" href={item.link} target="_blank" rel="noreferrer">{item.company}, </a>{item.location}</div>
                                     <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">{item.time}</time>
                                     <div className="text-base font-normal">{item.institute}</div>
                                     <p className="mb-4 text-sm font-normal">{item.descriptions}</p>
